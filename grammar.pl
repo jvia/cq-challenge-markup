6134ba85f76b217f9e1bcdd842a0d0e73bc618c6
Jeremiah Via <jeremiah.via@gmail.com>
2011-06-11 12:37:05 +0100

    Define a bit more of the grammar.

--- grammar.pl
+++ grammar.pl
@@ -6,40 +6,79 @@ Markup.
 @author Jeremiah Via <jxv911@cs.bham.ac.uk>
 */
 
-%% Body grammar
-body --> heading.
-body --> paragraph.
-body --> [].
+
+%% This will be moved to a different file once the grammar is
+%% finished.
+read_file(File, Text) :-
+        see(File),
+        read_file1([], Text),
+        %% Use the disjunction to that the
+        %% stream is always closed.
+        close(File); close(File).
+
+read_file1(Accm, Text) :-
+        get_char(Char),
+        ( Char == end_of_file ->
+            Accm = Text
+        ;
+            append(Accm, [Char], New_Accm),
+            read_file1(New_Accm, Text)
+        ).
+
+/********************************************************************/
+/*                              Grammar                             */
+/********************************************************************/
+
+%% Handle the optinonal mode line 
+mode_line --> ['-*- mode: markup; -*-'], blank_line.
+mode_line --> [].
 
 
-%% Heading grammar
-heading --> "*", text, nl.
+%% Handles the headings
+h --> ['*'], h.
+h --> [].
 
 
-%% Paragraph
-paragraph --> text, nl, nl.
+%% Handles a paragraph
+p --> text, blank_line.
 
 
-%% Block of text
-text --> char, text.
+%% Handles groups of text
+text --> alpha, text.
+text --> digit, text.
+text --> whitespace, text.
+text --> punct, text.
 text --> [].
 
 
-%% Newline
-nl --> "\n".
+%% Blank line must be two newlines or newline and end_of_file marker.
+blank_line --> nl, extra_nl, ['end_of_file'].
+blank_line --> nl, nl, extra_nl.
 
 
-%% Character
-char --> [C],
-        {
-         %% Lower case
-         member(C, "abcdefghijklmnopqrstuvwxyz");
-         %% Upper case
-         member(C, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-         %% White space
-         member(C, " _");
-         %% Numbers
-         member(C, "1234567890");
-         %% Symbols
-         member(C, "!£$%^&*(),.;'`¬\"")
+extra_nl --> nl, extra_nl.
+extra_nl --> [].
+
+
+/********************************************************************/
+/*                              Lexicon                             */
+/********************************************************************/
+
+
+nl --> ['\n'].
+
+digit --> [C], { name(C, [V]), V >= 48, V =< 57 }.
+
+whitespace --> [' '] | ['\t'].
+
+punct --> ['.'] | ['?'] | ['!'], [','] | [';'].
+
+alpha -->
+        [C],
+        { name(C, [V]),
+          (
+            97 =< V, V =< 122
+          ;
+            65 =< V, V =< 90
+          )
         }.

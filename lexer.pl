8ed668216cd5f1777c23d37a310939c8d3ae2d4c
Jeremiah Via <jeremiah.via@gmail.com>
2011-07-11 21:28:50 +0100

    Add comments and cleanup the code.

--- lexer.pl
+++ lexer.pl
@@ -8,7 +8,6 @@
           ]).
 
 
-%% Lex markup environments
 token(Xs)     --> modeline, token(Xs).
 token([X|Xs]) --> markup(X), token(Xs).
 token([X|Xs]) --> escape(X), token(Xs).
@@ -16,7 +15,7 @@ token([X|Xs]) --> lookahead("*"), asterisk(0, X), token(Xs).
 token([X|Xs]) --> lookahead(" "), spaces(0, X), token(Xs).
 token([X|Xs]) --> lookahead("\n"), nl(0, X), token(Xs).
 token([X|Xs]) --> bracket(X), token(Xs).
-token([word(X)|Xs]) -->
+token([X|Xs]) -->
         lookahead([C]),
         { is_plain_char(C) },
         word(X),
@@ -27,42 +26,54 @@ token([eof]) --> [C], {eof(C)}.
 %% May be error if not from command line
 token([no-eof]) --> [].
 
-
+%% Note of a hash mark because it may become an ordered list item.
 hash --> "#".
+
+
+%% Note of a dash mark because it may become an ordered list item.
 dash --> "-".
 
 
-escape(esc(C)) --> "\\", char(C).
+%% Catch everything that may be a potentially escaped character.
+escape(esc(C)) --> "\\", [C].
 
 
+%% Count the number of consecutive spaces and group together for
+%% easier processing in the grammar.
 spaces(X, Y)  --> " ", {X1 is X + 1}, spaces(X1, Y).
 spaces(X, spaces(X)) --> [].
 
 
+%% Count the number of consecutive newlines and group together for
+%% easier processing in the grammar.
 nl(X, Y) --> "\n", {X1 is X + 1}, nl(X1, Y).
 nl(X, nl(X)) --> [].
 
 
+%% Count the number of consecutive asterisks and group together for
+%% easier processing in the grammar.
 asterisk(X, Y) --> "*", {X1 is X + 1}, asterisk(X1, Y).
 asterisk(X, asterisk(X)) --> [].
 
 
-word([X|Xs]) --> char(X),  word(Xs).
-word([]) --> lookahead(C), { is_word_end(C) } | [].
+%% Group together characters into words. This includes punctuation
+%% because they make no difference in this task.
+word(word(X)) --> word1(X).
+word1([X|Xs]) --> char(X),  word1(Xs).
+word1([]) --> lookahead(C), { is_word_end(C) } | [].
 
 
+%% Tokenize a character which has no syntactic potential.
 char(C) --> [C], { is_plain_char(C) }.
 
 
-bracket(open(Bracket))  -->
-        [Bracket], {member(Bracket, "([{<")}.
-bracket(close(Bracket)) -->
-        [Bracket], {member(Bracket, ")]}>")}.
+%% Make a note of brackets since they tend to have syntactic
+%% importance.
+bracket(open(Bracket))  --> [Bracket], {member(Bracket, "([{<")}.
+bracket(close(Bracket)) --> [Bracket], {member(Bracket, ")]}>")}.
 
 
-
-
-%% Markup
+%% Catch markup here to make easier processing in the grammar.
 markup(italic) --> "\\i",    bracket(open("{")).
 markup(bold)   --> "\\b",    bracket(open("{")).
 markup(code)   --> "\\code", bracket(open("{")).
@@ -72,8 +83,12 @@ markup(note)   --> "\\note", bracket(open("{")).
 %% List of characters of potential syntactic importance
 special("([{<)]}>#-*").
 
-eof(-1).
+%% Catch the modeline so we can ignore it.
 modeline --> "-*- mode: markup; -*-".
+
+
+%% End of file character
+eof(-1).
 
 
 /*******************************/
@@ -96,6 +111,3 @@ is_word_end(C) :-
         name(' ', [C]) ;
         name('\n', [C]) ;
         -1 == C.
-
-
-        

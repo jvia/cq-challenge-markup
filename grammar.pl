4d50c2e799a8d440fe8d8e7a693d7c3ee418dcd8
Jeremiah Via <jeremiah.via@gmail.com>
2011-07-13 21:38:00 +0100

    Preliminary grammar definition.

--- grammar.pl
+++ grammar.pl
@@ -1,66 +1,24 @@
 %%% -*- Mode: Prolog; Module: user; -*-
-%%
-%%
-%% This module defines the grammar.
-%%
-%%
-%% TODO:
-%%   - [ ] Paragraph
-%%      - [ ] Can contain tagged markup
-%%      - [ ] Terminates with at least two newlines or
-%%            possibly an end_of_file marker
-%%   - [ ] Headers
-%%      - [ ] Special pragraph tha begins with '*'
-%%      - [ ] No limit to '*'
-%%      - [ ] Should be able to keep track of the level in the
-%%            hierarchy
-%%      - [ ] Besides '*', they function the same as paragraphs
-%%   - [ ] Block quotes
-%%      - [ ] Two spaces relative to the indentation level
-%%      - [ ] Ends with end_of_file or less indentation
-%%      - [ ] can contain pragraphs
-%%   - [ ] Verbatim section
-%%      - [ ] Three spaces relative to the enclosing section
-%%      - [ ] All text captured exactly as is
-%%   - [ ] Lists
-%%      - [ ] Two spaces of indentation followed by '-' or '#'
-%%      - [ ] '#' is ordered
-%%      - [ ] '-' unordered
-%%      - [ ] can be nested
-%%      - [ ] can contain paragraphs
-%%   - [ ] Links
-%%      - [ ] Unnamed linked '\url{}'
-%%      - [ ] Key link '[url|key]' which links to '[key|url]' at
-%%            the end of the document
-%%   - [ ] Tagged Markup
-%%      - [ ] italic: \i{}
-%%      - [ ] bold: \b{}
-%%      - [ ] code: \code{}
-%%      - [ ] note: \note{}
-%%      - [ ] Some markup is a subdocument rather than markup
-%%   - [ ] Escapes
-%%      - [ ] Backslash can escape any character at the location
-%%            where it has syntactic significance
-%%   - [ ] Mode line
-%%
-%% @author Jeremiah Via <jxv911@cs.bham.ac.uk>
 
 :- module(grammar,
-          [ eof/1               % enf_of_file
+          [ body/3              
           ]).
-:- dynamic indent/1.
+/********************************************************************/
+/*                             Grammar                              */
+/********************************************************************/
 
-indent(0).
+body([]) --> [eof] | [].
+
 
 
 /********************************************************************/
-/*                              Grammar                             */
+/*                         XML Transformer                          */
 /********************************************************************/
 
+ast_to_xml([], "<body/>").
+ast_to_xml(AST, XML) :-
+        ast_to_xml(AST, "<body>", XML).
 
-/********************************************************************/
-/*                              Lexicon                             */
-/********************************************************************/
 
-
-eof --> [-1].
+ast_to_xml([], Accm, XML) :-
+        append(Accm, "</body.", XML).
--- unit_tests.pl
+++ unit_tests.pl
@@ -6,9 +6,319 @@
 :- use_module(lexer).
 
 
-test_lexer :-
-        run_tests(lexer),
-        test_report(fixme).
+/********************************************************************/
+/*                             Lexer Tests                          */
+/********************************************************************/
+:- begin_tests(lexer).
+
+test('01_empty.txt',
+     [ setup(lexer_load('01_empty', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('02_simple_paragraph',
+     [ setup(lexer_load('02_simple_paragraph', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('03_multiline_paragraph',
+     [ setup(lexer_load('03_multiline_paragraph', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('04_two_paragraphs',
+     [ setup(lexer_load('04_two_paragraphs', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('05_several_multiline_paragraphs',
+     [ setup(lexer_load('05_several_multiline_paragraphs', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('06_header',
+     [ setup(lexer_load('06_header', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('07_headers',
+     [ setup(lexer_load('07_headers', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('08_crazy_header',
+     [setup(lexer_load('08_crazy_header', Text, FileTokens)),
+      true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('09_headers_and_paragraphs',
+     [setup(lexer_load('09_headers_and_paragraphs', Text, FileTokens)),
+      true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('10_blockquote',
+     [ setup(lexer_load('10_blockquote', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('11_multiline_blockquote',
+     [ setup(lexer_load('11_multiline_blockquote', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('12_multi_paragraph_blockquote',
+     [ setup(lexer_load('12_multi_paragraph_blockquote', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('13_paragraphs_and_blockquotes',
+     [ setup(lexer_load('13_paragraphs_and_blockquotes', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('14_simple_verbatim',
+     [ setup(lexer_load('14_simple_verbatim', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('15_useful_verbatim',
+     [ setup(lexer_load('15_useful_verbatim', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('16_verbatim_with_indentation',
+     [ setup(lexer_load('16_verbatim_with_indentation', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('17_verbatim_first_line_extra_indented',
+     [ setup(lexer_load('17_verbatim_first_line_extra_indented', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('18_verbatim_special_xml_chars',
+     [ setup(lexer_load('18_verbatim_special_xml_chars', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('19_numbered_list',
+     [ setup(lexer_load('19_numbered_list', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('20_bulleted_list',
+     [ setup(lexer_load('20_bulleted_list', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('21_multiparagraph_list_items',
+     [ setup(lexer_load('21_multiparagraph_list_items', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('22_nested_lists',
+     [ setup(lexer_load('22_nested_lists', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('23_tagged_markup',
+     [ setup(lexer_load('23_tagged_markup', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('24_note_subdocument.txt',
+     [ setup(lexer_load('24_note_subdocument', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('25_multiparagraph_note',
+     [ setup(lexer_load('25_multiparagraph_note', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+ 
+test('26_note_with_blockquote',
+     [ setup(lexer_load('26_note_with_blockquote', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('27_note_with_lists',
+     [ setup(lexer_load('27_note_with_lists', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('28_required_escapes',
+     [ setup(lexer_load('28_required_escapes', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('29_optional_escapes',
+     [ setup(lexer_load('29_optional_escapes', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('30_escaped_header',
+     [ setup(lexer_load('30_escaped_header', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('31_escaped_numbered_list_marker',
+     [ setup(lexer_load('31_escaped_numbered_list_marker', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('32_escaped_bullet_list_marker',
+     [ setup(lexer_load('32_escaped_bullet_list_marker', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('33_escapes_not_needed',
+     [ setup(lexer_load('33_escapes_not_needed', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('34_modeline',
+     [ setup(lexer_load('34_modeline', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('35_links',
+     [ setup(lexer_load('35_links', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('instructions',
+     [ setup(lexer_load('instructions', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+test('markup-spec',
+     [ setup(lexer_load('markup-spec', Text, FileTokens)),
+       true(Tokens == FileTokens)
+     ]) :-
+        phrase(token(Tokens), Text, []), !.
+
+:- end_tests(lexer).
+
+
+/********************************************************************/
+/*                            Grammar Tests                         */
+/********************************************************************/
+:- begin_tests(grammar).
+
+test('01_empty',
+     [ setup(grammar_load('01_empty', Tokens, File_XML)),
+       true(XML == File_XML),
+       fixme('not implemented')
+     ]) :-
+        phrase(body(XML), Tokens, []), !.
+
+:-end_tests(grammar).
+
+
+
+/********************************************************************/
+/*                            Helper Code                           */
+/********************************************************************/
+
+
+
+read_file(File, Text) :-
+        see(File),
+        read_file1([], Text),
+        %% Use the disjunction to that the
+        %% stream is always closed.
+        seen; seen.
+
+read_file1(Accm, Text) :-
+        get_code(Char),
+        ( Char == -1 ->
+          append(Accm, [-1], Text)
+        ;
+          append(Accm, [Char], New_Accm),
+          read_file1(New_Accm, Text)
+        ).
+
+read_xml(File, Text) :-
+        see(File),
+        read_xml1([], Text),
+        %% Use the disjunction to that the
+        %% stream is always closed.
+        seen; seen.
+
+read_xml1(Accm, Text) :-
+        get_code(Char),
+        ( Char == -1 ->
+          Accm = Text
+        ;
+          append(Accm, [Char], New_Accm),
+          read_xml1(New_Accm, Text)
+        ).
+
+lexer_load(Case, Text, FileTokens) :-
+        case(Case, Name),
+        append("tests/", Name, Path),
+        %% Load text
+        append(Path, ".txt", TextPathString),
+        string_to_atom(TextPathString, TextPath),
+        read_file(TextPath, Text),
+        %% Load lexer tokens
+        append(Path, ".tokens", TokenPathString),
+        string_to_atom(TokenPathString, TokenPath),
+        read_list(TokenPath, FileTokens).
+
+grammar_load(Case, Tokens, XML) :-
+        case(Case, Name),
+        append("tests/", Name, Path),
+        %% Load tokens
+        append(Path, ".tokens", TokenPathString),
+        string_to_atom(TokenPathString, TokenPath),
+        read_list(TokenPath, Tokens),
+        %% Load XML
+        append(Path, ".xml", XmlPathString),
+        string_to_atom(XmlPathString, XmlPath),
+        read_xml(XmlPath, XML).
+        
+
+read_list(File, List) :-
+        see(File),
+        read(List),
+        seen ; seen.
 
 
 case('01_empty',                              "01_empty").
@@ -48,265 +358,3 @@ case('34_modeline',                           "34_modeline").
 case('35_links',                              "35_links").
 case('instructions',                          "instructions").
 case('markup-spec',                           "markup-spec").
-
-
-:- begin_tests(lexer).
-test('01_empty.txt',
-     [ setup(load('01_empty', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('02_simple_paragraph',
-     [ setup(load('02_simple_paragraph', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('03_multiline_paragraph',
-     [ setup(load('03_multiline_paragraph', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('04_two_paragraphs',
-     [ setup(load('04_two_paragraphs', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('05_several_multiline_paragraphs',
-     [ setup(load('05_several_multiline_paragraphs', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('06_header',
-     [ setup(load('06_header', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('07_headers',
-     [ setup(load('07_headers', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('08_crazy_header',
-     [setup(load('08_crazy_header', Text, FileTokens)),
-      true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('09_headers_and_paragraphs',
-     [setup(load('09_headers_and_paragraphs', Text, FileTokens)),
-      true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('10_blockquote',
-     [ setup(load('10_blockquote', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('11_multiline_blockquote',
-     [ setup(load('11_multiline_blockquote', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('12_multi_paragraph_blockquote',
-     [ setup(load('12_multi_paragraph_blockquote', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('13_paragraphs_and_blockquotes',
-     [ setup(load('13_paragraphs_and_blockquotes', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('14_simple_verbatim',
-     [ setup(load('14_simple_verbatim', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('15_useful_verbatim',
-     [ setup(load('15_useful_verbatim', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('16_verbatim_with_indentation',
-     [ setup(load('16_verbatim_with_indentation', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('17_verbatim_first_line_extra_indented',
-     [ setup(load('17_verbatim_first_line_extra_indented', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('18_verbatim_special_xml_chars',
-     [ setup(load('18_verbatim_special_xml_chars', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('19_numbered_list',
-     [ setup(load('19_numbered_list', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('20_bulleted_list',
-     [ setup(load('20_bulleted_list', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('21_multiparagraph_list_items',
-     [ setup(load('21_multiparagraph_list_items', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('22_nested_lists',
-     [ setup(load('22_nested_lists', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('23_tagged_markup',
-     [ setup(load('23_tagged_markup', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('24_note_subdocument.txt',
-     [ setup(load('24_note_subdocument', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('25_multiparagraph_note',
-     [ setup(load('25_multiparagraph_note', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
- 
-test('26_note_with_blockquote',
-     [ setup(load('26_note_with_blockquote', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('27_note_with_lists',
-     [ setup(load('27_note_with_lists', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('28_required_escapes',
-     [ setup(load('28_required_escapes', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('29_optional_escapes',
-     [ setup(load('29_optional_escapes', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('30_escaped_header',
-     [ setup(load('30_escaped_header', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('31_escaped_numbered_list_marker',
-     [ setup(load('31_escaped_numbered_list_marker', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('32_escaped_bullet_list_marker',
-     [ setup(load('32_escaped_bullet_list_marker', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('33_escapes_not_needed',
-     [ setup(load('33_escapes_not_needed', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('34_modeline',
-     [ setup(load('34_modeline', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('35_links',
-     [ setup(load('35_links', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('instructions',
-     [ setup(load('instructions', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-test('markup-spec',
-     [ setup(load('markup-spec', Text, FileTokens)),
-       true(Tokens == FileTokens)
-     ]) :-
-        phrase(token(Tokens), Text, []), !.
-
-:- end_tests(lexer).
-
-
-/** Helper code **/
-
-read_file(File, Text) :-
-        see(File),
-        read_file1([], Text),
-        %% Use the disjunction to that the
-        %% stream is always closed.
-        seen; seen.
-
-read_file1(Accm, Text) :-
-        get_code(Char),
-        ( Char == -1 ->
-          append(Accm, [-1], Text)
-        ;
-          append(Accm, [Char], New_Accm),
-          read_file1(New_Accm, Text)
-        ).
-
-load(Case, Text, FileTokens) :-
-        case(Case, Name),
-        append("tests/", Name, Path),
-        %% Load text
-        append(Path, ".txt", TextPathString),
-        string_to_atom(TextPathString, TextPath),
-        read_file(TextPath, Text),
-        %% Load lexer tokens
-        append(Path, ".tokens", TokenPathString),
-        string_to_atom(TokenPathString, TokenPath),
-        read_list(TokenPath, FileTokens).
-
-read_list(File, List) :-
-        see(File),
-        read(List),
-        seen ; seen.
